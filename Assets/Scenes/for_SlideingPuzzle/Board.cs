d87c622edefb1741bfe8af31988fd426f556950e
Presto529 <polaris_p0529@naver.com>
2023-06-14 21:32:04 +0900

    Sliding Puzzle 업로드 및 수정
    1. Sliding 퍼즐 업로드 및 오류 수정
    + 퍼즐 정상 작동 확인 완료
    ++ 추후 추가로 확인 예정
    
    2. 메인 화면 option 버튼 및 미완성 UI 삭제

---
+++ Assets/Scenes/for_SlideingPuzzle/Board.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject tilePrefab;                              // 숫자 타일 프리팹
+	[SerializeField]
+	private Transform tilesParent;                          // 타일이 배치되는 "Board" 오브젝트의 Transform
+
+	private List<Tile> tileList;							// 생성한 타일 정보 저장
+
+	private Vector2Int puzzleSize = new Vector2Int(4, 4);       // 4x4 퍼즐
+	private float neighborTileDistance = 62;					// 인접한 타일 사이의 거리, 별도로 계산할 수도 있다.
+
+	public Vector3 EmptyTilePosition { set; get; }				// 빈 타일의 위치
+
+	private IEnumerator Start()
+	{
+		tileList = new List<Tile>();
+
+		SpawnTiles();
+
+		UnityEngine.UI.LayoutRebuilder.ForceRebuildLayoutImmediate(tilesParent.GetComponent<RectTransform>());
+
+		// 현재 프레임이 종료될 때까지 대기
+		yield return new WaitForEndOfFrame();
+
+		// tileList에 있는 모든 요소의 SetCorrectPosition() 메소드 호출
+		tileList.ForEach(x => x.SetCorrectPosition());
+
+		StartCoroutine("OnSuffle");
+	}
+
+	private void SpawnTiles()
+	{
+		for (int y = 0; y < puzzleSize.y; ++y)
+		{
+			for (int x = 0; x < puzzleSize.x; ++x)
+			{
+				GameObject clone = Instantiate(tilePrefab, tilesParent);
+				Tile tile = clone.GetComponent<Tile>();
+
+				tile.Setup(this, puzzleSize.x * puzzleSize.y, y * puzzleSize.x + x + 1);
+
+				tileList.Add(tile);
+			}
+		}
+	}
+
+	private IEnumerator OnSuffle()
+	{
+		float current = 0;
+		float percent = 0;
+		float time = 1.5f;
+
+		while (percent < 1)
+		{
+			current += Time.deltaTime;
+			percent = current / time;
+
+			int index = Random.Range(0, puzzleSize.x * puzzleSize.y);
+			tileList[index].transform.SetAsLastSibling();
+
+			yield return null;
+		}
+
+		// 원래 셔플 방식은 다른 방식이었는데 UI, GridLayoutGroup을 사용하다보니 자식의 위치를 바꾸는 것으로 설정
+		// 그래서 현재 타일리스트의 마지막에 있는 요소가 무조건 빈 타일
+		EmptyTilePosition = tileList[tileList.Count - 1].GetComponent<RectTransform>().localPosition;
+	}
+
+	public void IsMoveTile(Tile tile)
+	{
+		if (Vector3.Distance(EmptyTilePosition, tile.GetComponent<RectTransform>().localPosition) == neighborTileDistance)
+		{
+			Vector3 goalPosition = EmptyTilePosition;
+
+			EmptyTilePosition = tile.GetComponent<RectTransform>().localPosition;
+
+			tile.OnMoveTo(goalPosition);
+		}
+	}
+
+	public void IsGameOver()
+    {
+		List<Tile> tiles = tileList.FindAll(x => x.IsCorrected == true);
+
+		Debug.Log("Correct Count : " + tiles.Count);
+		if ( tiles.Count == puzzleSize.x * puzzleSize.y - 1)
+        {
+			Debug.Log("GameClear");
+        }
+    }
+}
