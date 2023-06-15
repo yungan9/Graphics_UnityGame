b4ce9947404d8a2d93a146bbadf43f49a20e6faf
yungan9 <geyun105@naver.com>
2023-06-16 04:01:47 +0900

    Cube, Tilt 수정
    게임 - 메인 연결 완료

--- Assets/Scripts/tiltgame/MainScene.cs
+++ Assets/Scripts/tiltgame/MainScene.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneLoader : MonoBehaviour
+public class MainScene : MonoBehaviour
 {
     public void LoadScene(string sceneName = "outpost with snow")
     {
